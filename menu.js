5c12f4a6e474ef9d7c8555839401f49a1f4d3238
James-Kyprianou <129155769+James-Kyprianou@users.noreply.github.com>
2023-04-15 16:10:04 +0930

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+  var menu = document.getElementById("menu-items");
+  if (menu.style.display === "none") {
+  menu.style.display = "block";
+  } else {
+  menu.style.display = "none";
+  }
+}
+document.getElementById("menu-button").addEventListener("click", toggleMenu);
